bb31c458c73e72dd837cf4741ee3e163acb74501
astar313 <alimirza786313@gmail.com>
2021-03-08 00:11:28 +0530

    Update on 08marc2021

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model1.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -28,10 +34,17 @@ def predict_api():
     For direct API calls trought request
     '''
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    prediction = model  .predict([np.array(list(data.values()))])
 
     output = prediction[0]
     return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
